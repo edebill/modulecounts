55ea62795ba1e90b5312513956318da53474fa86
Erik DeBill <erik@debill.org>
2010-12-10 23:01:09 -0600

    explicitly cache pages for 5 minutes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,8 @@
 class IndexController < ApplicationController
   def index
     @repositories = Repository.order(:name)
+
+    response.headers['Cache-Control'] = 'public, max-age=300'
   end
 
 end
