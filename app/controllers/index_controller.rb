1e193fcc2601d5bd95aca0f50d28f0da70614db1
Erik DeBill <erik@debill.org>
2011-04-10 10:43:21 -0500

    be graceful if we don't already have data for today - heroku started running the daily cron a lot later and it's better to now show a blank column just because it hasn't run yet

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,7 @@
 class IndexController < ApplicationController
   def index
     @repositories = Repository.order(:name)
+    @latest = Count.order("record_date desc").limit(1).first
 
     response.headers['Cache-Control'] = 'public, max-age=300'
     respond_to do |format|
