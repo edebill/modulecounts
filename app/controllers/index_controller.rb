9d3f02ff084a93bce87eecb9b2c24ea04f8b5c1d
Erik DeBill <edebill@moxiesoft.com>
2011-12-14 08:02:29 -0600

    do a case-insensitive sort when we display our repositories, so we can properly refer to npm

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def index
-    @repositories = Repository.order(:name)
+    @repositories = Repository.all.sort { |a,b| a.name.downcase <=> b.name.downcase }
     @latest = Count.order("record_date desc").limit(1).first
 
     response.headers['Cache-Control'] = 'public, max-age=300'
