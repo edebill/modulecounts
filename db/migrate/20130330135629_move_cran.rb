897e391f679b1236ebf3ac1cbb21f3a72df5554f
Erik <erik@debill.org>
2013-03-30 09:41:08 -0500

    fix issue #6 - fix CRAN archive

---
+++ db/migrate/20130330135629_move_cran.rb
@@ -0,0 +1,30 @@
+class MoveCran < ActiveRecord::Migration
+  def self.up
+    cran = Repository.find_by_name('CRAN (R)')
+    cran.url = 'http://cran.rproject.org'
+    cran.save
+    
+    sampler = cran.sampler
+
+    sampler.data_url = 'http://cran.r-project.org/web/packages/'
+    sampler.regex = "features\\s+([\\d,]+)\\s+available packages"
+    sampler.save
+
+    cran.reload
+    cran.update_count
+    cran.update_stats
+
+  end
+
+  def self.down
+    cran = Repository.find_by_name('CRAN (R)')
+    cran.url = 'http://cran.opensourceresources.org'
+    cran.save
+
+    sampler = cran.sampler
+
+    sampler.data_url = "http://cran.opensourceresources.org/web/packages/"
+    sampler.regex = "features\\s+([\\d,]+)\\s+available packages"
+    sampler.save
+  end
+end
