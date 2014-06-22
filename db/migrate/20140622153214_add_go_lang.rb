9147d7005c068c21c514bf44ca322c1acf83bb1b
Erik DeBill <erik@massrelevance.com>
2014-06-22 10:44:20 -0500

    add GoLang

---
+++ db/migrate/20140622153214_add_go_lang.rb
@@ -0,0 +1,20 @@
+class AddGoLang < ActiveRecord::Migration
+  @name = 'GoDoc (Go)'
+  @url = 'http://godoc.org/-/index'
+  def self.up
+    r = Repository.new(:name => @name, :url => @url)
+    s = RegexSampler.new
+    s.data_url = @url
+    s.regex = 'Number of packages: (\d+)\.'
+    s.offset = 0
+    r.sampler = s
+
+    r.save!
+    r.update_count
+  end
+
+  def self.down
+    r = Repository.where(:name => @name).first
+    r.destroy
+  end
+end
