d037002a7c4106ec29b2b0afcfbb629644f853d5
Erik DeBill <erik@massrelevance.com>
2014-07-24 07:28:43 -0500

    revert database.yml change and change precompile setting

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Modulecounts
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.assets.initialize_on_precompile = false
   end
 end
