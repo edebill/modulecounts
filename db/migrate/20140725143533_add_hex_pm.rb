fb69a5d38bc1665c51f67de2809a455156b8c5cb
Erik DeBill <erik@massrelevance.com>
2014-07-25 09:44:23 -0500

    add Hex.pm (Elixir/Erlang)

---
+++ db/migrate/20140725143533_add_hex_pm.rb
@@ -0,0 +1,20 @@
+class AddHexPm < ActiveRecord::Migration
+  def up
+    @repo_name = 'Hex.pm (Elixir/Erlang)'
+    @url = 'https://hex.pm/'
+    r = Repository.new(:name => @repo_name, :url => @url)
+    s = RegexSampler.new
+    s.data_url = "https://hex.pm"
+    s.regex = '(\d+)<\/div>\s*<div[^>]*>packages'
+    s.offset = 0
+    r.sampler = s
+
+    r.save!
+  end
+
+  def down
+    @repo_name = 'Hex.pm (Elixir/Erlang)'
+    r = Repository.where(:name => @repo_name).first
+    r.destroy
+  end
+end
