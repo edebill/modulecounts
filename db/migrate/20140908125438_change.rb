8e97b49af3c7a42b2e485fda236dc19d51bb2cb8
Erik DeBill <erik@massrelevance.com>
2014-09-08 07:58:12 -0500

    pull bower stats from new URL

---
+++ db/migrate/20140908125438_change.rb
@@ -0,0 +1,13 @@
+class Change < ActiveRecord::Migration
+  def up
+    r = Repository.find_by_name "Bower (JS)"
+    sampler = r.sampler
+
+    sampler.data_url = "https://bower-server-etl.herokuapp.com/api/1/data/all"
+    sampler.parse_sampler_configuration
+    sampler.save
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140725143533) do
+ActiveRecord::Schema.define(:version => 20140908125438) do
 
   create_table "counts", :force => true do |t|
     t.integer  "repository_id"
