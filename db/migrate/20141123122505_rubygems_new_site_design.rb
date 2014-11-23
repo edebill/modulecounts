d876316e8124844a10904741f91ab7cf10dcdf28
Erik DeBill <erik@massrelevance.com>
2014-11-23 07:25:44 -0600

    update for rubygems.org new site design

---
+++ db/migrate/20141123122505_rubygems_new_site_design.rb
@@ -0,0 +1,19 @@
+class RubygemsNewSiteDesign < ActiveRecord::Migration
+  def up
+    rubygems = Repository.where(:name => "Rubygems.org").first
+    sampler = rubygems.sampler
+
+    sampler.data_url = "http://rubygems.org/stats"
+    sampler.regex = "Total Gems[^<]*<[^<]*stat__count\">([0-9,]+)<"
+    sampler.save!
+  end
+
+  def down
+    rubygems = Repository.where(:name => "Rubygems.org").first
+    sampler = rubygems.sampler
+
+    sampler.data_url = "http://rubygems.org"
+    sampler.regex = "{\"regex\":\"of ([\\\\d,]+) gems cut since\"}"
+    sampler.save!
+  end
+end
