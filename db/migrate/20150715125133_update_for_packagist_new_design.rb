4ed6ecf4ae7ee508a3fbb0df5d1659a5d0edaca4
Erik DeBill <erik@debill.org>
2015-07-15 08:04:50 -0500

    new page design for Packagist

---
+++ db/migrate/20150715125133_update_for_packagist_new_design.rb
@@ -0,0 +1,17 @@
+class UpdateForPackagistNewDesign < ActiveRecord::Migration
+  def up
+    repo_name =  'Packagist (PHP)'
+    repo = Repository.where(name: repo_name).first
+    sampler = repo.sampler
+    sampler.regex = "Packages registered<\\/dt>[^>]+>(\\d[^<]+)<\\/dd"
+    sampler.save!
+  end
+
+  def down
+    repo_name =  'Packagist (PHP)'
+    repo = Repository.where(name: repo_name).first
+    sampler = repo.sampler
+    sampler.regex = "(\\d+\\s+\\d+) packages registered"
+    sampler.save!
+  end
+end
