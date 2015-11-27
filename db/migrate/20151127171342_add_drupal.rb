5f2bd1e8a770e70a4b645f92cab44a5feb7e0281
Erik DeBill <erik@debill.org>
2015-11-27 11:18:57 -0600

    add Drupal repository

---
+++ db/migrate/20151127171342_add_drupal.rb
@@ -0,0 +1,23 @@
+class AddDrupal < ActiveRecord::Migration
+  def up
+    r = Repository.new(name: repo_name,
+                       url: "https://www.drupal.org/project/project_module",
+                       graph: false)
+    s = RegexSampler.new
+    s.data_url = "https://www.drupal.org/project/project_module"
+    s.regex = ">([0-9,]+) Modules match your search"
+    s.offset = 0
+    r.sampler = s
+
+    r.save!
+  end
+
+  def down
+    r = Repository.where(:name => repo_name).first
+    r.destroy
+  end
+
+  def repo_name
+    "Drupal (php)"
+  end
+end
