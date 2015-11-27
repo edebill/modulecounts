73cd197bb5943562fe00af7f271cc5855cb0745f
Erik DeBill <erik@debill.org>
2015-11-27 11:12:25 -0600

    add Perl 6 Ecosystem

---
+++ db/migrate/20151127170059_add_perl_6.rb
@@ -0,0 +1,23 @@
+class AddPerl6 < ActiveRecord::Migration
+  def up
+    r = Repository.new(name: repo_name,
+                       url: "http://modules.perl6.org/",
+                       graph: false)
+    s = RegexSampler.new
+    s.data_url = "http://modules.perl6.org/total"
+    s.regex = "(\\d+)"
+    s.offset = 0
+    r.sampler = s
+
+    r.save!
+  end
+
+  def down
+    r = Repository.where(:name => repo_name).first
+    r.destroy
+  end
+
+  def repo_name
+    "Perl 6 Ecosystem (perl 6)"
+  end
+end
