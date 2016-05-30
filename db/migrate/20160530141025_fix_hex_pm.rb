c2386ced7b7bc89de23523ef79143b6525c04af7
Erik DeBill <erik@debill.org>
2016-05-30 09:20:09 -0500

    fix Hex.pm after site redesign

---
+++ db/migrate/20160530141025_fix_hex_pm.rb
@@ -0,0 +1,9 @@
+class FixHexPm < ActiveRecord::Migration
+  def change
+    r = Repository.where(name: "Hex.pm (Elixir/Erlang)").first
+    sampler = r.sampler
+    sampler.data_url = "https://hex.pm/packages"
+    sampler.regex = ">(\\d+) Results Found"
+    sampler.save
+  end
+end
