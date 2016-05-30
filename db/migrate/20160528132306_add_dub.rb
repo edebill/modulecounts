4fb7be1545d01ada43ebf2c9a4dfce863201223f
Erik DeBill <erik@debill.org>
2016-05-30 09:06:41 -0500

    add dub/dlang thanks to Hiroki Noda (https://github.com/kubo39/modulecounts/commit/8082ddf3e0ba081659d6e6432b21517ddaf93c66)

---
+++ db/migrate/20160528132306_add_dub.rb
@@ -0,0 +1,20 @@
+class AddDub < ActiveRecord::Migration
+  def up
+    @name = 'DUB (dlang)'
+    @url = 'https://code.dlang.org'
+    r = Repository.new(:name => @name, :url => @url)
+    s = RegexSampler.new
+    s.data_url = @url
+    s.regex = '<p>Found (\d+) packages.<\/p>'
+    s.offset = 0
+    r.sampler = s
+    r.save!
+  end
+
+  def down
+    @name = 'DUB (dlang)'
+
+    r = Repository.where(:name => @name).first
+    r.destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408130459) do
+ActiveRecord::Schema.define(version: 20160528132306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
